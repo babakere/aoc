eb468549350ca9502cdba6fb56f5fa420495a6aa
evan <evan@eroam-114-99.wifi.wmin.ac.uk>
2023-04-13 15:01:59 +0100

    first commit through terminal

---
+++ aoc/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.scss';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
