60a95967a02b8629676f2fd5eba2c6953b1584c6
ImranBary <imranfeisalAB@gmail.com>
2023-05-03 16:46:31 +0100

    Developed register and patient record pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import { Page,Footer } from "govuk-react";
+import { Page, Footer } from "govuk-react";
 import Main from "./pages/MainPage_ali";
 import Deregister from "./pages/deregister_ali";
 import Register from "./pages/register_imran";
+import PatientRecord from "./pages/patient_record_imran";
 import Info from "./info";
 import Login from "./pages/secondLogin_evan";
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./header";
 import LoginSelection from "./pages/loginpage_evan";
@@ -14,17 +15,15 @@ function App() {
     <div className="App">
       <Page header={<Header></Header>}>
         <Routes>
-      {/* Evans code */}
-          <Route path="/" element={<Info/>}/>
+          {/* Evans code */}
+          <Route path="/" element={<Info />} />
           <Route path="/loginselection" element={<LoginSelection />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/main" element={<Main/>}/>
+          <Route path="/main" element={<Main />} />
           <Route path="/deregister" element={<Deregister />} />
           <Route path="/register" element={<Register />} />
-
-
+          <Route path="/patientRecord" element={<PatientRecord />} />
         </Routes>
-
       </Page>
       <Footer></Footer>
     </div>
@@ -32,7 +31,6 @@ function App() {
 }
 
 export default App;
-
 
 /* import YourComponent from "./path/of/your/component";
 
--- src/pages/register_imran.js
+++ src/pages/register_imran.js
@@ -1,9 +1,51 @@
 import React from "react";
 import Header from "../header";
-
+import InputField from "@govuk-react/input-field";
+import DateField from "@govuk-react/date-field";
+import { Button, Heading } from "govuk-react";
+import FileUpload from "@govuk-react/file-upload";
 function Register() {
   <Header></Header>;
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <Heading>Register Page</Heading>
+      <div className="input-container">
+        <div className="reg-left">
+          <InputField>First Name</InputField>
+          <InputField>Middle Name</InputField>
+          <InputField>Last Name</InputField>
+          <DateField></DateField>
+          <InputField
+            input={{
+              autoComplete: "email",
+              name: "group1",
+              type: "email",
+            }}
+          >
+            Tel No
+          </InputField>
+          <FileUpload>Upload Photo</FileUpload>
+        </div>
+        <div className="reg-right">
+          <InputField>Address</InputField>
+          <InputField>Street Name</InputField>
+          <InputField>Post Code</InputField>
+          <InputField>NHS Number</InputField>
+          <InputField
+            input={{
+              autoComplete: "email",
+              name: "group1",
+              type: "email",
+            }}
+          >
+            Email
+          </InputField>
+          <InputField>Password</InputField>
+        </div>
+      </div>
+      <Button>Submit</Button>
+    </div>
+  );
 }
 
 export default Register;
