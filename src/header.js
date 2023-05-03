e3a8c6539e0b5905c4c91fbf1128bbfd9ebe8592
ImranBary <imranfeisalAB@gmail.com>
2023-05-03 14:26:31 +0100

    Updates to nav bar component

--- src/header.js
+++ src/header.js
@@ -1,23 +1,21 @@
 import React from "react";
-import {TopNav} from "govuk-react"
-import {Link} from "govuk-react"
+import { TopNav } from "govuk-react";
+import { Link } from "govuk-react";
+
 const Header = () => {
   return (
     <TopNav
-      children={
-        <div>
-          <Link href="about" >
-            about
-          </Link>
-          <Link href="location">
-            location</Link>
-          <Link href="loginselection" >
-            Login
-          </Link>
-         
-        </div>
+      company={
+        <TopNav.NavLink href="/main" target="new">
+          AOC
+        </TopNav.NavLink>
       }
-    ></TopNav>
+    >
+      <TopNav.NavLink href="about">About</TopNav.NavLink>
+      <TopNav.NavLink href="location">Location</TopNav.NavLink>
+      <TopNav.NavLink href="loginselection">Login</TopNav.NavLink>
+      <TopNav.NavLink href="register">Register</TopNav.NavLink>
+    </TopNav>
   );
 };
 
