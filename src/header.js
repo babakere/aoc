b3c9a6e2191a0b223405ae044b7244142c1f80b9
ImranBary <imranfeisalAB@gmail.com>
2023-05-06 15:18:35 +0100

    fixed nav bar styling

--- src/header.js
+++ src/header.js
@@ -2,24 +2,40 @@ import React from "react";
 import { TopNav } from "govuk-react";
 import { Link } from "govuk-react";
 import { useLocation } from "react-router-dom";
+import { SearchIcon } from "govuk-react";
 const Header = () => {
   const location = useLocation();
   return (
     <TopNav
-
-
       company={
+        <TopNav.Anchor href="/main" target="new">
+          <TopNav.IconTitle icon={<SearchIcon height="32" width="36" />}>
+            GOV.UK
+          </TopNav.IconTitle>
+        </TopNav.Anchor>
+      }
+      serviceTitle={
         <TopNav.NavLink href="/main" target="new">
-          AOC
+          AOC Surgery
         </TopNav.NavLink>
       }
     >
       {location.pathname != "/patientRecord" && (
         <>
-          <TopNav.NavLink href="about">About</TopNav.NavLink>
-          <TopNav.NavLink href="location">Location</TopNav.NavLink>
-          <TopNav.NavLink href="loginselection">Login</TopNav.NavLink>
-          <TopNav.NavLink href="register">Register</TopNav.NavLink>
+          <div style={{ width: "200%" }}>
+            <TopNav.NavLink href="about" style={{ marginRight: "7%" }}>
+              About
+            </TopNav.NavLink>
+            <TopNav.NavLink href="location" style={{ margin: "3%" }}>
+              Location{" "}
+            </TopNav.NavLink>
+            <TopNav.NavLink href="loginselection" style={{ margin: "3%" }}>
+              Login{" "}
+            </TopNav.NavLink>
+            <TopNav.NavLink href="register" style={{ margin: "3%" }}>
+              Register{" "}
+            </TopNav.NavLink>
+          </div>
         </>
       )}
       {location.pathname == "/patientRecord" && (
