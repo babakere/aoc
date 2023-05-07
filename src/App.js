8b7979df55a95e9880e597d4d8b0b08b96077abb
ImranBary <imranfeisalAB@gmail.com>
2023-05-08 00:32:48 +0100

    Fixed login backedn

--- src/App.js
+++ src/App.js
@@ -8,15 +8,12 @@ import Info from "./info";
 import Login from "./pages/secondLogin_evan";
 import Patient from "./pages/patient_mo";
 
-
-import {Routes,Route, useNavigate} from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Doctor from "./pages/DoctorPage_evan";
 import View from "./pages/viewAppointment_evan";
 import Edit1 from "./pages/changeRecord_evan";
 import DoctorPat from "./pages/docPatientRecord_evan";
 import Edit from "./pages/editPatientPage_evan";
-
-
 
 import Header from "./header";
 
@@ -27,50 +24,39 @@ import MainPage from "./pages/MainPage_ali";
 import { createContext, useState } from "react";
 export const useContext = createContext();
 
-
-
 function App() {
-const [islogged, setIsLogged] = useState(
-  localStorage.getItem("isLogged") || "false"
-)
-
-
+  const [islogged, setIsLogged] = useState(
+    localStorage.getItem("isLogged") || "false"
+  );
 
   return (
     <div className="App">
+      <div className="pages">
+        <useContext.Provider value={{ islogged, setIsLogged }}>
+          <Page header={<Header></Header>}>
+            <Routes>
+              {/* Evans code */}
+              <Route path="/loginselection" element={<LoginSelection />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/main" element={<Main />} />
+              <Route path="/patient" element={<Patient />} />
 
-<div className="pages">
-      <useContext.Provider value={{islogged, setIsLogged}}>
+              <Route path="/deregister" element={<Deregister />} />
+              <Route path="/mainpage" element={<MainPage />} />
+              <Route path="/register" element={<Register />} />
 
-      <Page  header={<Header></Header>}>
-        <Routes>
-          {/* Evans code */}
-          <Route path="/" element={<Info />} />
-          <Route path="/loginselection" element={<LoginSelection />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/patient" element={<Patient />} />
-          
-          <Route path="/deregister" element={<Deregister />} />
-          <Route path="/mainpage" element={< MainPage/>} />
-          <Route path="/register" element={<Register />} />
+              <Route path="/patientRecord" element={<PatientRecord />} />
 
-          <Route path="/patientRecord" element={<PatientRecord />} />
-
-          <Route path="/doctor" element={<Doctor/>} />
-          <Route path="/view" element={<View/>}/>
-          <Route path="/edit1" element={<Edit1/>}/>
-          <Route path="/doctorPat" element={<DoctorPat/>}/>
-          <Route path="edit" element={<Edit />}/>
-
-
-        </Routes>
-
-      </Page>
-      </useContext.Provider>
-</div>
+              <Route path="/doctor" element={<Doctor />} />
+              <Route path="/view" element={<View />} />
+              <Route path="/edit1" element={<Edit1 />} />
+              <Route path="/doctorPat" element={<DoctorPat />} />
+              <Route path="edit" element={<Edit />} />
+            </Routes>
+          </Page>
+        </useContext.Provider>
+      </div>
       <Footer className="footer"></Footer>
-
     </div>
   );
 }
