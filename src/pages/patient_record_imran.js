6265aa2ce39005eb24e3e492619abf23040724c9
ImranBary <imranfeisalAB@gmail.com>
2023-05-08 16:28:52 +0100

    page routing developments

--- src/pages/patient_record_imran.js
+++ src/pages/patient_record_imran.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Button, Heading, Table, Link, Input } from "govuk-react";
-
+import { Button, Heading, Table, Link, Input, BackLink } from "govuk-react";
+import { useNavigate } from "react-router-dom";
 function PatientRecord() {
   const [patientDetails, setPatientDetails] = useState({});
   const [vaccineRecords, setVaccineRecords] = useState([]);
   const [editing, setEditing] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     const email = localStorage.getItem("email");
@@ -63,109 +64,109 @@ function PatientRecord() {
   };
 
   return (
-    <div className="patient-record-page">
+    <div>
+      <BackLink href="#" onClick={() => navigate(-1)}>
+        Back
+      </BackLink>
       <Heading>Patient Record</Heading>
-      <div className="p-details">
-        <Table caption="Patient Details">
-          <Table.Row>
-            <Table.CellHeader>NHS Number</Table.CellHeader>
-            <Table.Cell>{patientDetails.NHSNumber}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>Forename</Table.CellHeader>
-            <Table.Cell>
-              {editing.Name ? (
-                <Input
-                  value={patientDetails.Name}
-                  onChange={(e) => handleInputChange(e, "Name")}
-                />
-              ) : (
-                patientDetails.Name
-              )}
-            </Table.Cell>
-            <Table.Cell>
-              {editing.Name ? (
-                <Button
-                  onClick={() => {
-                    stopEditing("Name");
-                    updatePatientDetails(
-                      patientDetails.Email,
-                      "Name",
-                      patientDetails.Name
-                    );
-                  }}
-                >
-                  Update
-                </Button>
-              ) : (
-                <Link onClick={() => startEditing("Name")}>Edit</Link>
-              )}
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>Surname</Table.CellHeader>
-            <Table.Cell>
-              {editing.Surname ? (
-                <Input
-                  value={patientDetails.Surname}
-                  onChange={(e) => handleInputChange(e, "Surname")}
-                />
-              ) : (
-                patientDetails.Surname
-              )}
-            </Table.Cell>
-            <Table.Cell style={{ width: "5vw" }}>
-              {editing.Surname ? (
-                <Button
-                  onClick={() => {
-                    stopEditing("Surname");
-                    updatePatientDetails(
-                      patientDetails.Email,
-                      "Surname",
-                      patientDetails.Surname
-                    );
-                  }}
-                >
-                  Update
-                </Button>
-              ) : (
-                <Link onClick={() => startEditing("Surname")}>Edit</Link>
-              )}
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>Date of Birth</Table.CellHeader>
-            <Table.Cell>{patientDetails.PersonDB}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>Gender</Table.CellHeader>
-            <Table.Cell>{patientDetails.Gender}</Table.Cell>
-          </Table.Row>
-        </Table>
 
-        <div className="p-vacc-record">
-          <Table caption="Vaccine Records">
-            {vaccineRecords.map((record, index) => (
-              <React.Fragment key={index}>
-                <Table.Row>
-                  <Table.CellHeader>
-                    Date of Vaccination: {record.VaccinationDate}
-                  </Table.CellHeader>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell>
-                    {`${record.DoseNo} ${record.VaccineManufacturer} ${
-                      record.VaccineType
-                    } ${record.VaccineBatchNumber} ${
-                      record.Booster ? "Booster" : ""
-                    }`}
-                  </Table.Cell>
-                </Table.Row>
-              </React.Fragment>
-            ))}
-          </Table>
-        </div>
-      </div>
+      <Table caption="Patient Details">
+        <Table.Row>
+          <Table.CellHeader>NHS Number</Table.CellHeader>
+          <Table.Cell>{patientDetails.NHSNumber}</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.CellHeader>Forename</Table.CellHeader>
+          <Table.Cell>
+            {editing.Name ? (
+              <Input
+                value={patientDetails.Name}
+                onChange={(e) => handleInputChange(e, "Name")}
+              />
+            ) : (
+              patientDetails.Name
+            )}
+          </Table.Cell>
+          <Table.Cell>
+            {editing.Name ? (
+              <Button
+                onClick={() => {
+                  stopEditing("Name");
+                  updatePatientDetails(
+                    patientDetails.Email,
+                    "Name",
+                    patientDetails.Name
+                  );
+                }}
+              >
+                Update
+              </Button>
+            ) : (
+              <Link onClick={() => startEditing("Name")}>Edit</Link>
+            )}
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.CellHeader>Surname</Table.CellHeader>
+          <Table.Cell>
+            {editing.Surname ? (
+              <Input
+                value={patientDetails.Surname}
+                onChange={(e) => handleInputChange(e, "Surname")}
+              />
+            ) : (
+              patientDetails.Surname
+            )}
+          </Table.Cell>
+          <Table.Cell style={{ width: "5vw" }}>
+            {editing.Surname ? (
+              <Button
+                onClick={() => {
+                  stopEditing("Surname");
+                  updatePatientDetails(
+                    patientDetails.Email,
+                    "Surname",
+                    patientDetails.Surname
+                  );
+                }}
+              >
+                Update
+              </Button>
+            ) : (
+              <Link onClick={() => startEditing("Surname")}>Edit</Link>
+            )}
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.CellHeader>Date of Birth</Table.CellHeader>
+          <Table.Cell>{patientDetails.PersonDB}</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.CellHeader>Gender</Table.CellHeader>
+          <Table.Cell>{patientDetails.Gender}</Table.Cell>
+        </Table.Row>
+      </Table>
+
+      <Table caption="Vaccine Records">
+        {vaccineRecords.map((record, index) => (
+          <React.Fragment key={index}>
+            <Table.Row>
+              <Table.CellHeader>
+                Date of Vaccination: {record.VaccinationDate}
+              </Table.CellHeader>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                {`${record.DoseNo} ${record.VaccineManufacturer} ${
+                  record.VaccineType
+                } ${record.VaccineBatchNumber} ${
+                  record.Booster ? "Booster" : ""
+                }`}
+              </Table.Cell>
+            </Table.Row>
+          </React.Fragment>
+        ))}
+      </Table>
     </div>
   );
 }
