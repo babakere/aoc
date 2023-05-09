de8cceabe56da851e60cbb47494e45ec3a29dea8
ali <w1823773@westminster.ac.uk>
2023-05-09 11:20:33 +0100

    comments

--- src/pages/MainPage_ali.js
+++ src/pages/MainPage_ali.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../ali.css";
-import { Button, LeadParagraph, InputField, ErrorText, Paragraph,H5} from "govuk-react";
+import { Button, LeadParagraph, InputField, ErrorText, Paragraph,H5, Heading, H1, H2, OrderedList, ListItem,} from "govuk-react";
 import { useState } from "react";
 import picture1 from "../images/1.jpg"
 import picture2 from "../images/2.jpg"
@@ -11,26 +11,25 @@ import picture11 from "../images/11.jpg"
 import picture12 from "../images/12.png"
 
 
-// import { Panel } from "govuk-react";
-// import ButtonToTop from "./backToTop"
 
-// import Image from "./";
+function MainPage() {  {/*This line defines a function component named MainPage*/}
 
-function MainPage() {
-
+{/*These lines use the useState hook to create state variables for the component.
+  The variables are phone, fullname, message, email, and submitted, and their initial values are empty strings or false.*/}
   const [phone, setPhone] = useState("");
   const [fullname, setFullname] = useState("");
   const [message, setMessage] = useState("");
   const [email, setEmail] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  {/*This line defines a function named handleSubmit that will be called when the form is submitted.*/}
+  const handleSubmit = (e) => {  
+    e.preventDefault(); 
     setSubmitted(true)
-    if (!phone &&
+    if (!phone &&     
       !fullname &&
       !message &&
-      !email ) {
+      !email ) {   {/*If none of the form fields have been filled out, nothing happens.*/}
      
     } else {
       // Handle form submission here
@@ -40,75 +39,76 @@ function MainPage() {
   };
   return (
     <div className="main-page" >
-      <div className="welcome" >
+     <LeadParagraph className="par">
 
-    <LeadParagraph className="par">We are providing system that enable people to book appiontment across England.<br/>
-                                  <h2>Access fast online AOC GP services</h2>
-                                  Your health should always be your Top priority and concern,regardless of how busy your schedule is.<br/>
-                                   <p><h3>Register in just three easy steps:<br/>
-                                              1- Click register online<br/>
-                                              2- Fill out your Details<br/>
-                                              3- Access all our online GP services
-                                              </h3></p> 
-    <Button className="button">Login</Button>
-    <Button className="button">Register</Button>
+      <H2>Access fast online AOC GP services</H2>
+      We are providing system that enable people to book appiontment across England.<br/>
+      Register in just three easy steps:<br/>                                                                         
+  <OrderedList>
+        <ListItem>
+            Click register online
+        </ListItem>
+        <ListItem>
+            Fill out your Details
+        </ListItem>
+        <ListItem>
+            Access all our online GP services
+        </ListItem>
+</OrderedList>
+<Button className="button">Login</Button>
+<Button className="button">Register</Button>
     </LeadParagraph>
-      </div>
 
-      <br/>
-      <div><br/>
-      <h1 >AboutUs</h1>
+  <div><br/>
+      <H1>AboutUs</H1>
         <div className="grid1">
-        <img  className="picture" src={picture1}></img>
-            {/* <img src={picture}  ></img> */}
-            {/* <img src={Image}></img> */}
+            <img  className="picture" src={picture1}></img>
             <img className="picture" src={picture2}></img>
             <img className="picture" src={picture4}></img>
         </div>
         <div className="grid">
-          {/* title */}
-          
           <div className="box">
-          <p>Dog</p>
-            <p className="Paragrapgh">ghghsgdjgfasjhgjahajh<br/>sagfajsghfajhgfjhgasdf<br/> hxjhsfhgakjdghjkhgkjghf</p>
+          <Heading size="S">Experienced Doctor</Heading>
+            <p className="Paragrapgh">Our doctors are highly trained and experienced professionals<br/>
+                                      who possess the necessary knowledge and skills to provide patients<br/>
+                                      with accurate and up-to-date information about vaccination.</p>
           </div>
           <div className="box">
-
-            <p>c-dog</p>
-            <p className="Paragrapgh">ghghsgdjgfasjhgjahajh<br/>sagfajsghfajhgfjhgasdf<br/> hxjhsfhgakjdghjkhgkjghf</p>
+            <Heading size="S">Ensuring the safety & efficacy of vaccines</Heading>
+            <p className="Paragrapgh">our doctors conduct thorough laboratory testing to ensure <br/>
+                          that the vaccine is safe and effective.</p>
           </div>
           <div className="box">
-
-            <p>B-dog</p>
-            <p className="Paragrapgh">ghghsgdjgfasjhgjahajh<br/>sagfajsghfajhgfjhgasdf<br/> hxjhsfhgakjdghjkhgkjghf</p>
+            <Heading size="S">Expert Guidance and Support for Vaccinations</Heading>
+            <p className="Paragrapgh">Our doctors are knowledgeable about the different types of vaccines available<br/>
+                                      including those for children and adults,<br/> 
+                                      and can provide guidance on the appropriate vaccination schedule for each individual.</p>
           </div>
-          {/* paragraph */}  
         </div>
 
-        {/* <img className="hos"src="https://www.rbht.nhs.uk/sites/default/files/Images/Harefield%20Hospital%203%20v2.jpg"></img> */}
-        <di>
-          <img className="vac"src={picture12}></img>
-        </di>
-      </div>
+    <div> {/*Vaccination LOGo*/}
+      <img className="vac"src={picture12}></img>
+    </div>
+  </div>
      
-        <h1>Location</h1>
-     
+      <Heading size="L" >Location</Heading>  {/*This is the location part*/}
         <div className="Location" >
           <img src={picture11}></img>
-          <p ><h1>Nearst Tube Station</h1><h4>Great Portland Street Station:</h4> Euston Rd,London NW1<br/><h4>Goodge Street Station:</h4>75 Tottenham Ct Rd, London W1P 9PA<br/><h4>Warren Street Station
-: </h4>London NW1 3AA<br/>
-{/* <a class="btn btn-primary btn-sm" href="https://goo.gl/maps/7L5wc" target="_blank" title="View the surgery location on a Google map">view map</a> */}
-</p>        
+          <p ><Heading size="M" >Nearst Tube Station</Heading>
+          <H5 size="S">Great Portland Street Station:</H5> Euston Rd,London NW1<br/>
+          <H5 size="S">Goodge Street Station:</H5>75 Tottenham Ct Rd, London W1P 9PA<br/>
+          <H5 size="S">Warren Street Station</H5>London NW1 3AA<br/>
+            <a class="btn btn-primary btn-sm" href="https://goo.gl/maps/H9QVf2e62ohUFhzo9?coh=178571&entry=tt" target="_blank" title="View the surgery location on a Google map">view map</a> 
+          </p>        
         </div>
-      <>
-        <h1>Testimonials</h1>
+      <Heading size="L">Testimonials</Heading>  {/*This is the testimonials part*/}
         <div className="feedback">
           <div className="box1">
             <H5>Testemonial-1</H5>
           <p>"A wonderful GP practice, the best I've ever been registered with in UK.<br></br>
               Supportive and proactive doctor, excellent nurses and that rarity: kind and effiecent...."</p>
           </div>
-          <div className="box1">
+          <div className="box1">  {/*the css have been applied to make the paragraph nice*/}
           <H5>Testemonial-2</H5>
           <p>"Good experience with friendly receptionst and doctor. "</p>
           </div>
@@ -119,40 +119,36 @@ function MainPage() {
               Try to answer calls and patients at the same time then you will understand the process.<br/>
               Keep up the good work and thank  you for giving me what I need"</p>
           </div>
-         
         </div>
-      </>
       
-  
-      
-        <h1>Blog</h1>
+    
+        <Heading>Blog</Heading>
         <div className="blog">
           <img className="picture"src={picture9}></img>
           <img className="picture"src={picture10}></img>
           
-          
           <div className="blog1">
-            <p>Post-Vaccination Health Guidelines</p>
+            <H5>Post-Vaccination Health Guidelines</H5>
             <p>Stay vigilant: Continue to follow recommended health guidelines such as wearing<br/>
                a mask and practicing social distancing even after vaccination<br/>
-                until the pandemic is under control.</p>
+              until the pandemic is under control.</p>
           </div>
            <div className="blog1">
-              <p>Consult your healthcare provider before vaccination</p>
+              <H5>Consult your Doctor before Vaccination</H5>
               <p>Talk to your healthcare provider: If you have any medical conditions or concerns,<br/>
                   talk to your healthcare provider before getting vaccinated.</p>
            </div>
-         
         </div>
      
-      
-        <h1>Contact Us</h1>
-      <div className="contact">
+       
+        <div className="contact"> 
+          <Heading size="M">Contact Us</Heading>
         <InputField 
                 type="text"
                 value={fullname}
                 onChange={(e) => setFullname(e.target.value)}>FullName</InputField>
-        {!fullname && submitted?(<ErrorText>Required</ErrorText>):null}
+        {/*This line displays an error message within an ErrorText component if variable is falsy (empty string or null) and submitted is true.*/}
+        {!fullname && submitted?(<ErrorText>Required</ErrorText>):null}  
         <InputField
                 type="text"
                 value={phone}
@@ -163,16 +159,19 @@ function MainPage() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}>Email</InputField>
                 {!email && submitted?(<ErrorText>Required</ErrorText>):null}
-        </div>
+         </div> 
         <div>
-          <h5>Message</h5>
+          <H1 size="S">Message</H1>
           <textarea  onChange={(e) => setMessage(e.target.value)}></textarea>
           {!message && submitted?(<ErrorText>Required</ErrorText>):null}
         </div>
+        {/*This line creates a Button component with an onClick handler that calls the handleSubmit function when the button is clicked.*/}
         <Button  onClick={(e) => handleSubmit(e)}>Submit</Button>
-    
-<a href="#ok">Back To Top</a>
-      {/* <ButtonToTop>top</ButtonToTop> */}
+        <br/>
+        <Button buttonShadowColour="#f49938">
+        <a href="#ok">Back To Top</a>
+        </Button>
+
     </div>
   );
 }
--- src/pages/deregister_ali.js
+++ src/pages/deregister_ali.js
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { Heading, Button, LoadingBox } from "govuk-react";
-
+{/*This code defines a Deregister function component that uses the useState hook to 
+  define two pieces of state: loading and deregistered. Both are initially set to false.*/}
 function Deregister() {
   const [loading, setLoading] = useState(false);
   const [deregistered, setDeregistered] = useState(false);
 
-  const handleDeregister = () => {
-    const email = localStorage.getItem("email");
-
+  const handleDeregister = () => {      {/*This code defines a handleDeregister function that is called when the user clicks the "Deregister" button.*/}
+    const email = localStorage.getItem("email"); {/* It first retrieves the user's email from local storage, then sets the loading state to true.*/}
     setLoading(true);
     fetch(`http://localhost:8000/deregisterPatient.php?email=${email}`)
       .then((response) => response.text())
       .then((data) => {
-        console.log(data);
+        console.log(data);  {/*when the request completes, it logs the response data to the console and sets the deregistered state to true*/}
         setDeregistered(true);
       });
   };
-
+{/*I uses the useEffect hook to watch for changes to the deregistered state. */}
   useEffect(() => {
     if (deregistered) {
-      setTimeout(() => {
+      setTimeout(() => { {/*When deregistered becomes true, i sets a timeout for 2 seconds.*/}
         setLoading(false);
-        window.location.href = "/main";
+        window.location.href = "/main"; {/*When the timeout completes, i sets the loading state back to false and redirects the user to the /main page.*/}
       }, 2000);
     }
   }, [deregistered]);
-
+{/*This code returns a LoadingBox component that displays a spinner when the loading state is true*/}
   return (
     <LoadingBox
       loading={loading}
@@ -35,6 +35,7 @@ function Deregister() {
       timeIn={800}
       timeOut={200}
     >
+      {/* button's onClick event is set to call the handleDeregister function when clicked.*/}
       <div>
         <Heading> Deregister page</Heading>
         <Button onClick={handleDeregister}>Deregister</Button>
